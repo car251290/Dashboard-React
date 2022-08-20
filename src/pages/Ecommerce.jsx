d02afbe5aaccf1f3f3e5d4f6258b4c3e064c038b
Javier  Carreño <35305955+car251290@users.noreply.github.com>
2022-08-20 12:56:57 -0500

    Add files via upload

--- src/pages/Ecommerce.jsx
+++ src/pages/Ecommerce.jsx
@@ -1,8 +1,22 @@
 import React from 'react'
+import { BsCurrencyDollar } from 'react-icons/bs';
+import {GoPrimitiveDot} from 'react-icons/go';
+import { Stacked, Pie, Button, LineChart, SparkLine } from '../components';
+import { earningData, medicalproBranding, recentTransactions, weeklyStats, dropdownData, SparklineAreaData, ecomPieChartData } from '../data/dummy';
+import { useStateContext } from '../contexts/ContextProvider';
+
+
 
 const Ecommerce = () => {
   return (
-    <div>Ecommerce</div>
+    <div className='mt-12'>
+      <div className='flex flex-wrap lg:flex-nowrap justify-center'>
+        <div className='bg-white dark:text-gray-200 dark:bg-secondary-dark-bg h-44 rounded-x1 w-full lg:w-80 p-8 pt-9 mbg-hero-pattern bg-no-repat bg-cover bg-center'>
+          
+        </div>
+      </div>
+      Ecommerce
+      </div>
   )
 }
 
